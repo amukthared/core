25e99191ce8d2be5fc8e3949d72726ce5d7f5b3d
amukthared <amukthared@gmail.com>
2023-10-06 00:12:20 +0200

    refactored get many

--- homeassistant/components/recorder/table_managers/event_types.py
+++ homeassistant/components/recorder/table_managers/event_types.py
@@ -53,54 +53,57 @@ class EventTypeManager(BaseLRUTableManager[EventTypes]):
         """
         return self.get_many((event_type,), session)[event_type]
 
-    def get_many(
-        self, event_types: Iterable[str], session: Session, from_recorder: bool = False
-    ) -> dict[str, int | None]:
-        """Resolve event_types to event_type_ids.
+   def get_many(self, event_types: Iterable[str], session: Session, from_recorder: bool = False) -> dict[str, int | None]:
+    """Resolve event_types to event_type_ids.
 
-        This call is not thread-safe and must be called from the
-        recorder thread.
-        """
-        results: dict[str, int | None] = {}
-        missing: list[str] = []
-        non_existent: list[str] = []
-
-        for event_type in event_types:
-            if (event_type_id := self._id_map.get(event_type)) is None:
-                if event_type in self._non_existent_event_types:
-                    results[event_type] = None
-                else:
-                    missing.append(event_type)
-
-            results[event_type] = event_type_id
-
-        if not missing:
-            return results
-
-        with session.no_autoflush:
-            for missing_chunk in chunked(missing, SQLITE_MAX_BIND_VARS):
-                for event_type_id, event_type in execute_stmt_lambda_element(
-                    session, find_event_type_ids(missing_chunk), orm_rows=False
-                ):
-                    results[event_type] = self._id_map[event_type] = cast(
-                        int, event_type_id
-                    )
-
-        if non_existent := [
-            event_type for event_type in missing if results[event_type] is None
-        ]:
-            if from_recorder:
-                # We are already in the recorder thread so we can update the
-                # non-existent event types directly.
-                for event_type in non_existent:
-                    self._non_existent_event_types[event_type] = None
-            else:
-                # Queue a task to refresh the event types since its not
-                # thread-safe to do it here since we are not in the recorder
-                # thread.
-                self.recorder.queue_task(RefreshEventTypesTask(non_existent))
-
+    This call is not thread-safe and must be called from the
+    recorder thread.
+    """
+    results, missing = self._get_initial_results_and_missing(event_types)
+    
+    if not missing:
         return results
+
+    with session.no_autoflush:
+        self._fetch_missing_event_ids_from_db(session, missing, results)
+    
+    non_existent = [event_type for event_type in missing if results[event_type] is None]
+    
+    if non_existent:
+        self._handle_non_existent_event_types(non_existent, from_recorder)
+    
+    return results
+
+def _get_initial_results_and_missing(self, event_types: Iterable[str]) -> Tuple[dict[str, int | None], list[str]]:
+    """Get initial results and a list of event types that are missing from the cache."""
+    results = {}
+    missing = []
+    
+    for event_type in event_types:
+        event_type_id = self._id_map.get(event_type)
+        
+        if event_type_id is None and event_type in self._non_existent_event_types:
+            results[event_type] = None
+        elif event_type_id is None:
+            missing.append(event_type)
+        
+        results[event_type] = event_type_id
+    
+    return results, missing
+
+def _fetch_missing_event_ids_from_db(self, session: Session, missing: list[str], results: dict[str, int | None]) -> None:
+    """Fetch missing event type ids from the database and update the cache and results."""
+    for missing_chunk in chunked(missing, SQLITE_MAX_BIND_VARS):
+        for event_type_id, event_type in execute_stmt_lambda_element(session, find_event_type_ids(missing_chunk), orm_rows=False):
+            results[event_type] = self._id_map[event_type] = cast(int, event_type_id)
+
+def _handle_non_existent_event_types(self, non_existent: list[str], from_recorder: bool) -> None:
+    """Handle event types that do not exist in the database."""
+    if from_recorder:
+        for event_type in non_existent:
+            self._non_existent_event_types[event_type] = None
+    else:
+        self.recorder.queue_task(RefreshEventTypesTask(non_existent))
 
     def add_pending(self, db_event_type: EventTypes) -> None:
         """Add a pending EventTypes that will be committed at the next interval.
