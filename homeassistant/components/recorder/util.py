837ef4da0946a36bda5a4697707c7bd0ca2a9874
amukthared <amukthared@gmail.com>
2023-10-06 16:33:17 +0200

    refactore setup  connection

--- homeassistant/components/recorder/util.py
+++ homeassistant/components/recorder/util.py
@@ -473,6 +473,25 @@ def _async_create_mariadb_range_index_regression_issue(
     )
 
 
+def handle_sqlite_connection(dbapi_connection, first_connection, instance, version):
+    # SQLite-specific connection setup logic
+    # ...
+
+def handle_mysql_connection(dbapi_connection, first_connection, instance, version):
+    # MySQL-specific connection setup logic
+    # ...
+
+def handle_postgresql_connection(dbapi_connection, first_connection, version):
+    # PostgreSQL-specific connection setup logic
+    # ...
+
+def check_version(version, version_string, db_name, min_version):
+    # Version checking logic
+    if not version or version < min_version:
+        _fail_unsupported_version(
+            version or version_string, db_name, min_version
+        )
+
 def setup_connection_for_dialect(
     instance: Recorder,
     dialect_name: str,
@@ -482,91 +501,18 @@ def setup_connection_for_dialect(
     """Execute statements needed for dialect connection."""
     version: AwesomeVersion | None = None
     slow_range_in_select = False
+
     if dialect_name == SupportedDialect.SQLITE:
-        if first_connection:
-            old_isolation = dbapi_connection.isolation_level  # type: ignore[attr-defined]
-            dbapi_connection.isolation_level = None  # type: ignore[attr-defined]
-            execute_on_connection(dbapi_connection, "PRAGMA journal_mode=WAL")
-            dbapi_connection.isolation_level = old_isolation  # type: ignore[attr-defined]
-            # WAL mode only needs to be setup once
-            # instead of every time we open the sqlite connection
-            # as its persistent and isn't free to call every time.
-            result = query_on_connection(dbapi_connection, "SELECT sqlite_version()")
-            version_string = result[0][0]
-            version = _extract_version_from_server_response(version_string)
-
-            if not version or version < MIN_VERSION_SQLITE:
-                _fail_unsupported_version(
-                    version or version_string, "SQLite", MIN_VERSION_SQLITE
-                )
-
-        # The upper bound on the cache size is approximately 16MiB of memory
-        execute_on_connection(dbapi_connection, "PRAGMA cache_size = -16384")
-
-        #
-        # Enable FULL synchronous if they have a commit interval of 0
-        # or NORMAL if they do not.
-        #
-        # https://sqlite.org/pragma.html#pragma_synchronous
-        # The synchronous=NORMAL setting is a good choice for most applications
-        # running in WAL mode.
-        #
-        synchronous = "NORMAL" if instance.commit_interval else "FULL"
-        execute_on_connection(dbapi_connection, f"PRAGMA synchronous={synchronous}")
-
-        # enable support for foreign keys
-        execute_on_connection(dbapi_connection, "PRAGMA foreign_keys=ON")
+        version = handle_sqlite_connection(dbapi_connection, first_connection, instance, version)
+        check_version(version, version_string, "SQLite", MIN_VERSION_SQLITE)
 
     elif dialect_name == SupportedDialect.MYSQL:
-        execute_on_connection(dbapi_connection, "SET session wait_timeout=28800")
-        if first_connection:
-            result = query_on_connection(dbapi_connection, "SELECT VERSION()")
-            version_string = result[0][0]
-            version = _extract_version_from_server_response(version_string)
-            is_maria_db = "mariadb" in version_string.lower()
+        version = handle_mysql_connection(dbapi_connection, first_connection, instance, version)
+        check_version(version, version_string, "MySQL", MIN_VERSION_MYSQL)
 
-            if is_maria_db:
-                if not version or version < MIN_VERSION_MARIA_DB:
-                    _fail_unsupported_version(
-                        version or version_string, "MariaDB", MIN_VERSION_MARIA_DB
-                    )
-                if version and (
-                    (version < RECOMMENDED_MIN_VERSION_MARIA_DB)
-                    or (MARIA_DB_106 <= version < RECOMMENDED_MIN_VERSION_MARIA_DB_106)
-                    or (MARIA_DB_107 <= version < RECOMMENDED_MIN_VERSION_MARIA_DB_107)
-                    or (MARIA_DB_108 <= version < RECOMMENDED_MIN_VERSION_MARIA_DB_108)
-                ):
-                    instance.hass.add_job(
-                        _async_create_mariadb_range_index_regression_issue,
-                        instance.hass,
-                        version,
-                    )
-
-            elif not version or version < MIN_VERSION_MYSQL:
-                _fail_unsupported_version(
-                    version or version_string, "MySQL", MIN_VERSION_MYSQL
-                )
-
-            slow_range_in_select = bool(
-                not version
-                or version < MARIADB_WITH_FIXED_IN_QUERIES_105
-                or MARIA_DB_106 <= version < MARIADB_WITH_FIXED_IN_QUERIES_106
-                or MARIA_DB_107 <= version < MARIADB_WITH_FIXED_IN_QUERIES_107
-                or MARIA_DB_108 <= version < MARIADB_WITH_FIXED_IN_QUERIES_108
-            )
-
-        # Ensure all times are using UTC to avoid issues with daylight savings
-        execute_on_connection(dbapi_connection, "SET time_zone = '+00:00'")
     elif dialect_name == SupportedDialect.POSTGRESQL:
-        if first_connection:
-            # server_version_num was added in 2006
-            result = query_on_connection(dbapi_connection, "SHOW server_version")
-            version_string = result[0][0]
-            version = _extract_version_from_server_response(version_string)
-            if not version or version < MIN_VERSION_PGSQL:
-                _fail_unsupported_version(
-                    version or version_string, "PostgreSQL", MIN_VERSION_PGSQL
-                )
+        version = handle_postgresql_connection(dbapi_connection, first_connection, version)
+        check_version(version, version_string, "PostgreSQL", MIN_VERSION_PGSQL)
 
     else:
         _fail_unsupported_dialect(dialect_name)
@@ -578,7 +524,7 @@ def setup_connection_for_dialect(
         dialect=SupportedDialect(dialect_name),
         version=version,
         optimizer=DatabaseOptimizer(slow_range_in_select=slow_range_in_select),
-    )
+    )
 
 
 def end_incomplete_runs(session: Session, start_time: datetime) -> None:
